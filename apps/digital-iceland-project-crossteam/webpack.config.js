aea663a1095d32b11e39c9d5bb642cdede8f0ab6
Alexey Tereshko <alexeytereshko@Mac.Home>
2025-05-06 14:33:44 +0100

    feature: setup webpack and ts configuration

--- apps/digital-iceland-project-crossteam/webpack.config.js
+++ apps/digital-iceland-project-crossteam/webpack.config.js
@@ -1,17 +1,36 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nrwlConfig = require('./../../libs/shared/webpack/nrwl-config')
 const { composePlugins, withNx } = require('@nx/webpack')
 const { withReact } = require('@nx/react')
 
-// Nx plugins for webpack.
-module.exports = composePlugins(
-  withNx(),
-  withReact({
-    // Uncomment this line if you don't want to use SVGR
-    // See: https://react-svgr.com/
-    // svgr: false
-  }),
-  (config) => {
-    // Update the webpack config as needed here.
-    // e.g. `config.plugins.push(new MyPlugin())`
-    return config
-  },
-)
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = composePlugins(withNx(), withReact(), nrwlConfig, (config) => {
+  // App specific config
+  config.stats.chunks = false
+  config.stats.modules = false
+
+  if (isDev) {
+    config.devtool = 'eval-cheap-module-source-map'
+  }
+
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    fs: false,
+    // "zlib_bindings": false,
+    process: require.resolve("process/browser"),
+    zlib: require.resolve("browserify-zlib"),
+    stream: require.resolve("stream-browserify"),
+    util: require.resolve("util"),
+    buffer: require.resolve("buffer"),
+    asset: require.resolve("assert"),
+  };
+
+  return {
+    ...config,
+    node: {
+      global: true,
+    },
+
+  }
+})
