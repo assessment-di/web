2e5cd58100ef453e985daa99448936a921c2b2f3
Aliaksei Tsiareshka <aliaksei.tsiareshka@itera.com>
2025-05-06 15:34:32 +0100

    feature: add home screen logo

--- apps/digital-iceland-project-web/src/app/App.tsx
+++ apps/digital-iceland-project-web/src/app/App.tsx
@@ -1,8 +1,15 @@
 'use client'
+import { Routes, Route, Link } from 'react-router-dom'
 import Details from './Details'
+import Home from './Home'
 
 export const App = () => {
-  return <Details />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/details" element={<Details />} />
+    </Routes>
+  )
 }
 
 export default App
