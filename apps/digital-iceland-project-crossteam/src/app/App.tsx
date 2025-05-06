8ead12892558bdd0cd6efee31c5526a2821b0bea
Aliaksei Tsiareshka <aliaksei.tsiareshka@itera.com>
2025-05-06 14:48:15 +0100

    feature: create Details page

--- apps/digital-iceland-project-crossteam/src/app/App.tsx
+++ apps/digital-iceland-project-crossteam/src/app/App.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { Box, Text } from '@island.is/island-ui/core'
+import Details from './Details'
 
 export function App() {
   return (
     <Box padding={4}>
       <Text variant="h1">Welcome to Digital Iceland</Text>
+      <Details />
     </Box>
   )
 }
