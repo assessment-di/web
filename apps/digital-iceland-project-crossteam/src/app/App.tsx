4fc773a03adbda1c6f88aa89056dcf23b42d7b26
Alexey Tereshko <alexeytereshko@Mac.Home>
2025-05-06 14:27:01 +0100

    initialise project

---
+++ apps/digital-iceland-project-crossteam/src/app/App.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Box, Text } from '@island.is/island-ui/core'
+
+export function App() {
+  return (
+    <Box padding={4}>
+      <Text variant="h1">Welcome to Digital Iceland</Text>
+    </Box>
+  )
+}
+
+export default App
