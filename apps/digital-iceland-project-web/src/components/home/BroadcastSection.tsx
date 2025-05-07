fe56f41c628ed2d6672337f5cc26d67d196dd78c
Aliaksei Tsiareshka <aliaksei.tsiareshka@itera.com>
2025-05-07 18:18:14 +0100

    feature: improve Broadcast section

--- apps/digital-iceland-project-web/src/components/home/BroadcastSection.tsx
+++ apps/digital-iceland-project-web/src/components/home/BroadcastSection.tsx
@@ -3,44 +3,48 @@ import { SimpleVideoPlayer } from '../SimpleVideoPlayer/SimpleVideoPlayer'
 
 export const BroadcastSection = () => {
   return (
-    <Box marginTop={6}>
-      <Box display="flex" alignItems="center" marginBottom={2}>
-        <Box
-          background="red100"
-          borderRadius="standard"
-          paddingX={2}
-          paddingY={1}
-          marginRight={2}
-        >
-          <Text color="red600" fontWeight="semiBold" variant="small">
-            Live
-          </Text>
-        </Box>
-        <Text variant="h5" as="h3">
-          Broadcast
-        </Text>
+    <Box
+      marginTop={6}
+      background="white"
+      borderRadius="large"
+      borderWidth="standard"
+      borderColor="blue200"
+      padding={3}
+      display="flex"
+      flexDirection={["column", "row"]}
+      style={{ gap: 32 }}
+    >
+      <Box style={{ flex: 1, minWidth: 280 }}>
+        <SimpleVideoPlayer
+          url="https://www.youtube.com/watch?v=YjkLqcRmL0M"
+          title="Althingi"
+        />
       </Box>
-      <Box
-        display="flex"
-        flexDirection={['column', 'row']}
-        style={{ gap: 32 }}
-      >
-        <Box style={{ flex: 1 }}>
-          <SimpleVideoPlayer
-            url="https://www.youtube.com/watch?v=YjkLqcRmL0M"
-            title="Althingi"
-          />
-        </Box>
-        <Box style={{ flex: 2 }}>
-          <Text>
-            The next parliamentary session will be on Wednesday, May 7 at 15:00
-          </Text>
-          <Box marginTop={2} display="flex" style={{ gap: 16 }}>
-            <IslandLink href="#">Other broadcast options</IslandLink>
-            <IslandLink href="#">Recordings</IslandLink>
+      <Box style={{ flex: 2, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+        <Box display="flex" alignItems="center" marginBottom={2}>
+          <Box
+            background="red100"
+            borderRadius="standard"
+            paddingX={2}
+            paddingY={1}
+            marginRight={2}
+          >
+            <Text color="red600" fontWeight="semiBold" variant="small">
+              Live
+            </Text>
           </Box>
+          <Text variant="h5" as="h3">
+            Broadcast
+          </Text>
+        </Box>
+        <Text>
+          The next parliamentary session will be on Wednesday, May 7 at 15:00
+        </Text>
+        <Box marginTop={2} display="flex" style={{ gap: 16 }}>
+          <IslandLink href="#">Other broadcast options</IslandLink>
+          <IslandLink href="#">Recordings</IslandLink>
         </Box>
       </Box>
     </Box>
   )
-} 
+}
