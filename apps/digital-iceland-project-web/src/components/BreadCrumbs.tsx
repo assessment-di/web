1315498fdc9f89d70426fcddcbd8a74c30406417
Aliaksei Tsiareshka <aliaksei.tsiareshka@itera.com>
2025-05-07 13:37:00 +0100

    feature: use library breadcrumbs

--- apps/digital-iceland-project-web/src/components/BreadCrumbs.tsx
+++ apps/digital-iceland-project-web/src/components/BreadCrumbs.tsx
@@ -1,5 +1,5 @@
-import { Box, Text } from '@island.is/island-ui/core'
-import { Link, useLocation } from 'react-router-dom'
+import { Breadcrumbs, Box } from '@island.is/island-ui/core'
+import { useLocation } from 'react-router-dom'
 
 const breadcrumbSegmentMap: Record<string, string> = {
   '': 'Home',
@@ -15,11 +15,15 @@ const getBreadcrumbs = (pathname: string) => {
   const pathnames = pathname.split('/').filter(Boolean)
   const crumbs = []
   let path = ''
-  crumbs.push({ path: '/', label: breadcrumbSegmentMap[''] })
+  crumbs.push({ title: breadcrumbSegmentMap[''], href: '/' })
   for (let i = 0; i < pathnames.length; i++) {
     path += '/' + pathnames[i]
-    const label = breadcrumbSegmentMap[pathnames[i]] || capitalize(pathnames[i])
-    crumbs.push({ path, label })
+    const title = breadcrumbSegmentMap[pathnames[i]] || capitalize(pathnames[i])
+    crumbs.push({
+      title,
+      href: path,
+      isCurrentPage: i === pathnames.length - 1,
+    })
   }
   return crumbs
 }
@@ -33,49 +37,8 @@ const BreadCrumbs = () => {
   }
 
   return (
-    <Box display="flex" alignItems="center" paddingTop={1} paddingBottom={3} paddingX={6}>
-      {crumbs.map((crumb, idx) => (
-        <Box
-          key={crumb.path}
-          display="flex"
-          alignItems="center"
-          marginLeft={idx !== 0 ? 2 : 0}
-        >
-          {idx !== 0 && (
-            <Box paddingX={1}>
-              <Text color="blue400" fontWeight="semiBold" aria-hidden="true">
-                •
-              </Text>
-            </Box>
-          )}
-          {idx === crumbs.length - 1 ? (
-            <Box
-              background="blue100"
-              borderRadius="large"
-              paddingX={3}
-              paddingY={1}
-              marginLeft={idx !== 0 ? 2 : 0}
-            >
-              <Text color="blue400" fontWeight="semiBold">
-                {crumb.label}
-              </Text>
-            </Box>
-          ) : (
-            <Link
-              to={crumb.path}
-              style={{
-                color: '#1662DD',
-                fontWeight: 600,
-                textDecoration: 'none',
-                fontSize: 20,
-                marginLeft: idx !== 0 ? 8 : 0,
-              }}
-            >
-              {crumb.label}
-            </Link>
-          )}
-        </Box>
-      ))}
+    <Box paddingTop={1} paddingBottom={3} paddingX={8}>
+      <Breadcrumbs items={crumbs} />
     </Box>
   )
 }
