50de7631243f849dcfbdf654ae9e5c9f5780aa38
Aliaksei Tsiareshka <aliaksei.tsiareshka@itera.com>
2025-05-07 19:40:26 +0100

    feature: improve mobile styles of legislation page

--- apps/digital-iceland-project-web/src/pages/Legislation.tsx
+++ apps/digital-iceland-project-web/src/pages/Legislation.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Box, Text } from '@island.is/island-ui/core'
+import { Box, Text, Stack } from '@island.is/island-ui/core'
 import { mockLaws } from '../mockData/legislation'
 import WebReader from '../components/WebReader'
 import { LegislationSearch } from '../components/legislation/LegislationSearch'
@@ -32,35 +32,62 @@ const Legislation = () => {
   )
 
   return (
-    <Box paddingY={6} paddingX={2} background="blue100">
+    <Box background="blue100" paddingY={[4, 6, 8]} paddingX={[2, 3, 4]}>
       <WebReader readId="legislation-content" />
-      <Box id="legislation-content">
-        <Text variant="h1" as="h1" marginBottom={3}>
-          Legislation
-        </Text>
-        <Box display="flex" flexDirection="row" alignItems="center">
-          <Box marginBottom={3}>
-            <LegislationSearch value={search} onChange={setSearch} />
-          </Box>
-          <Box marginBottom={3} marginLeft={2}>
-            <LegislationFilters
-              status={status}
-              onStatusChange={(newStatus) => {
-                setStatus(newStatus)
-                setPage(1)
-              }}
-            />
-          </Box>
+      <Box width="full" margin="auto" style={{ maxWidth: '1200px' }}>
+        <Box id="legislation-content">
+          <Stack space={4}>
+            <Box>
+              <Text variant="h1" as="h1" color="blue600">
+                Legislation
+              </Text>
+              <Text variant="intro" color="dark400" marginTop={2}>
+                Browse and search through current legislation and proposals
+              </Text>
+            </Box>
+
+            <Box 
+              background="white" 
+              padding={[3, 4]} 
+              borderRadius="large"
+              boxShadow="subtle"
+            >
+              <Stack space={3}>
+                <Box display="flex" flexDirection={['column', 'row']} alignItems={['stretch', 'center']}>
+                  <Box marginBottom={[3, 0]} width="full">
+                    <LegislationSearch value={search} onChange={setSearch} />
+                  </Box>
+                  <Box marginLeft={[0, 2]} width="full">
+                    <LegislationFilters
+                      status={status}
+                      onStatusChange={(newStatus) => {
+                        setStatus(newStatus)
+                        setPage(1)
+                      }}
+                    />
+                  </Box>
+                </Box>
+
+                <Box 
+                  background="white" 
+                  borderRadius="large"
+                  overflow="hidden"
+                >
+                  <LegislationTable laws={paginatedLaws} />
+                </Box>
+
+                <LegislationPagination
+                  currentPage={page}
+                  totalPages={totalPages}
+                  onPageChange={setPage}
+                  startIndex={startIndex}
+                  endIndex={Math.min(startIndex + itemsPerPage, filteredLaws.length)}
+                  totalItems={filteredLaws.length}
+                />
+              </Stack>
+            </Box>
+          </Stack>
         </Box>
-        <LegislationTable laws={paginatedLaws} />
-        <LegislationPagination
-          currentPage={page}
-          totalPages={totalPages}
-          onPageChange={setPage}
-          startIndex={startIndex}
-          endIndex={Math.min(startIndex + itemsPerPage, filteredLaws.length)}
-          totalItems={filteredLaws.length}
-        />
       </Box>
     </Box>
   )
