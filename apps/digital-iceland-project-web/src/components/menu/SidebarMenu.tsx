2575d25cc88c71ab5c922e60a4451b43a2061143
Aliaksei Tsiareshka <aliaksei.tsiareshka@itera.com>
2025-05-07 21:09:40 +0100

    feature: refactor style on SidebarMenu

--- apps/digital-iceland-project-web/src/components/menu/SidebarMenu.tsx
+++ apps/digital-iceland-project-web/src/components/menu/SidebarMenu.tsx
@@ -2,6 +2,14 @@ import { Box } from '@island.is/island-ui/core'
 import React from 'react'
 import MenuSection from './MenuSection'
 
+const styles = `
+  @media (max-width: 1100px) {
+    .sidebar-menu {
+      --sidebar-width: 90%;
+    }
+  }
+`
+
 interface MenuItem {
   label: string
   href: string
@@ -26,13 +34,15 @@ const SidebarMenu: React.FC<SidebarMenuProps> = ({
 }) => {
   return (
     <Box>
+      <style>{styles}</style>
       <Box
+        className="sidebar-menu"
         display="flex"
         flexDirection={['row', 'row', 'column']}
         columnGap={[3, 3, 0]}
         rowGap={[0, 0, 3]}
       >
-        <Box width="full">
+        <Box style={{ width: 'var(--sidebar-width, 100%)' }}>
           <MenuSection
             items={mainMenu}
             title={mainTitle}
@@ -41,7 +51,7 @@ const SidebarMenu: React.FC<SidebarMenuProps> = ({
             backgroundColor="blue100"
           />
         </Box>
-        <Box width="full">
+        <Box style={{ width: 'var(--sidebar-width, 100%)' }}>
           <MenuSection
             items={extraMenu}
             title={extraTitle}
