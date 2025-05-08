636c91dfa034081e78be84f9f50677e68616ec9c
Aliaksei Tsiareshka <aliaksei.tsiareshka@itera.com>
2025-05-08 10:12:04 +0100

    feature: refactor header

--- apps/digital-iceland-project-web/src/mockData/menu.ts
+++ apps/digital-iceland-project-web/src/mockData/menu.ts
@@ -2,16 +2,11 @@ export const mainMenu = [
   { label: 'Þingfundir og mál', href: '/legislation' },
   { label: 'Þingmenn', href: '/members' },
   { label: 'Nefndir', href: '/' },
-  { label: 'Alþjóðastarf', href: '/' },
   { label: 'Lagasafn', href: '/' },
   { label: 'Um Alþingi', href: '/' },
 ]
 
 export const extraMenu = [
-  { label: 'Þingmálaskrá ríkisstjórnarinnar', href: '/' },
   { label: 'Staða mála', href: '/' },
   { label: 'Fjárlög 2024', href: '/' },
-  { label: 'Lagafrumvörp', href: '/' },
-  { label: 'Þingsályktunartillögur', href: '/' },
-  { label: 'Þingmálaskrá ríkisstjórnarinnar', href: '/' },
 ]
